603429f730a00f67d6234cad66b66415cf6a54ad
KyleDeveaux <kyledeveaux@gmail.com>
2022-09-27 21:22:44 -0400

    twitter II
    Likes
    retweets
    writing tweets

---
+++ twitter_ios_starter/Twitter/tweetCellTableViewCell.swift
@@ -0,0 +1,91 @@
+//
+//  tweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Kyle Deveaux on 9/20/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class tweetCellTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    //favButton
+    @IBOutlet weak var favButton: UIButton!
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        let toBeFavorited = !favorited
+        if (toBeFavorited) {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (error) in
+                print("Favorite did not work \(error)")
+            })
+        } else {
+            TwitterAPICaller.client?.unFavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+
+            }, failure: { (error) in
+                print("Unfavorite Unsuccesful \(error)")
+            })
+        }
+    }
+    
+    
+    
+    //retweetButton
+    @IBOutlet weak var retweetButton: UIButton!
+    
+    @IBAction func retweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in
+            print("error is retweeting \(error)")
+        })
+    }
+    
+    
+    
+    
+    //making the button turn from gray to red
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    
+    
+    func setFavorite(_ isFavorited:Bool) {
+        favorited = isFavorited
+        if (favorited) {
+            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        } else {
+            favButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+        }
+    }
+    
+    
+    func setRetweeted(_ isRetweeted:Bool) {
+        if (isRetweeted) {
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        } else {
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
